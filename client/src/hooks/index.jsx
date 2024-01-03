9f112cd7d1bd10ac7f963ce7ab4325c050807a54
lacitadelle <jkomala99@gmail.com>
2024-01-03 14:02:06 +0700

    feat: made a custom hook for data fetching

---
+++ client/src/hooks/index.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+function useBlogPosts() {
+  const [posts, setPosts] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        setIsError(false);
+        setIsLoading(true);
+        const results = await axios("http://localhost:4000/posts");
+        setPosts(results.data.data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsError(true);
+      }
+    };
+
+    getPosts();
+  }, []);
+
+  return { posts, isError, isLoading };
+}
+
+export { useBlogPosts };
